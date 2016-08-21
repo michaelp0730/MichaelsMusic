e67e3ad0ba7a75dc93bbff7ab29a6cea9d07c284
Michael Pellegrini <mipelleg@microsoft.com>
2016-08-21 11:42:45 -0700

    First commit after setting up project in VS

---
+++ MichaelsMusic/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MichaelsMusic.Startup))]
+namespace MichaelsMusic
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
