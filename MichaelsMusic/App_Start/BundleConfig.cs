170405361fae56f5cd0fb7ed2850332a934c70b1
Michael Pellegrini <mipelleg@microsoft.com>
2016-08-22 21:08:28 -0700

    Replace bootstrap with Foundation

--- MichaelsMusic/App_Start/BundleConfig.cs
+++ MichaelsMusic/App_Start/BundleConfig.cs
@@ -9,23 +9,22 @@ namespace MichaelsMusic
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+                "~/Scripts/vendor/jquery-1.10.2.min.js"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+                "~/Scripts/vendor/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+            bundles.Add(new ScriptBundle("~/bundles/foundation").Include(
+                "~/Scripts/vendor/foundation.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/app").Include(
+                "~/Scripts/app.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                "~/Content/foundation.min.css",
+                "~/Content/app.css"));
         }
     }
 }
