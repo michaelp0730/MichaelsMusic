438829ff51c19667bd0e0784d7a9ee22fa091247
Michael Pellegrini <mipelleg@microsoft.com>
2016-08-24 22:00:26 -0700

    Create Item model

---
+++ MichaelsMusic/Models/Item.cs
@@ -0,0 +1,23 @@
+﻿using System.Web;
+
+namespace MichaelsMusic.Models
+{
+    public class Item
+    {
+        // TODO: Create enum for type property (ex: Guitar, Amp, Pedal)
+        public string Slug { get; set; }
+        public string DisplayName { get; set; }
+        public string BrandName { get; set; }
+        public string ModelName { get; set; }
+        public string ArtistName { get; set; }
+        public string Description { get; set; }
+        public string ShortDescription { get; set; }
+        public bool IsMine { get; set; }
+        public bool WasMine { get; set; }
+        public decimal Cost { get; set; }
+        public int YearBuilt { get; set; }
+        public IHtmlString Thumbnail { get; set; }
+        public IHtmlString FeaturedImage { get; set; }
+        public IHtmlString[] Images { get; set; }
+    }
+}
