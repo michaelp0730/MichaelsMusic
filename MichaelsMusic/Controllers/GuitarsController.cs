40f5591325aa037896b8a1a3e7fff33592edc2fe
Michael Pellegrini <mipelleg@microsoft.com>
2016-08-27 00:23:23 -0700

    Load guitars.json from JSON directory

--- MichaelsMusic/Controllers/GuitarsController.cs
+++ MichaelsMusic/Controllers/GuitarsController.cs
@@ -1,4 +1,8 @@
-﻿using System.Web.Mvc;
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Web.Mvc;
+using MichaelsMusic.Models;
+using Newtonsoft.Json;
 
 namespace MichaelsMusic.Controllers
 {
@@ -7,6 +11,13 @@ namespace MichaelsMusic.Controllers
         // GET: Guitars
         public ActionResult Index()
         {
+            var guitars = new List<Guitar>();
+
+            using (StreamReader sr = new StreamReader(Server.MapPath("~/JSON/guitars.json")))
+            {
+                guitars = JsonConvert.DeserializeObject<List<Guitar>>(sr.ReadToEnd());
+            }
+
             return View();
         }
     }
--- MichaelsMusic/Models/Item.cs
+++ MichaelsMusic/Models/Item.cs
@@ -7,19 +7,19 @@ namespace MichaelsMusic.Models
         // TODO: Create enum for type property (ex: Guitar, Amp, Pedal)
         public bool IsMine { get; set; }
         public bool WasMine { get; set; }
-        public IHtmlString FeaturedImage { get; set; }
-        public IHtmlString[] Images { get; set; }
-        public IHtmlString Thumbnail { get; set; }
         public int YearBuilt { get; set; }
         public Link[] Links { get; set; }
         public Link[] VideoLinks { get; set; }
         public string ArtistName { get; set; }
         public string BrandName { get; set; }
-        public IHtmlString Description { get; set; }
         public string Cost { get; set; }
+        public string Description { get; set; }
         public string DisplayName { get; set; }
+        public string FeaturedImage { get; set; }
+        public string[] Images { get; set; }
         public string ModelName { get; set; }
         public string ShortDescription { get; set; }
         public string Slug { get; set; }
+        public string Thumbnail { get; set; }
     }
 }
