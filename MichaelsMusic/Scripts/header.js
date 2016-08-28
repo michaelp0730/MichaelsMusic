a0452d4add983c8bbe4b8c76819c8e40167ffab3
Michael Pellegrini <mipelleg@microsoft.com>
2016-08-28 16:09:50 -0700

    Finish mobile menu

--- MichaelsMusic/Scripts/header.js
+++ MichaelsMusic/Scripts/header.js
@@ -1,14 +1,16 @@
 (function(global, $) {
     'use strict';
-    var $expandBtn = $('.mobile-expand'),
-        $topLevelMenuItems = $('.menu li').not('.submenu li, .branding, .mobile-expand');
-
-    $('body').on('click', $expandBtn, function(e) {
-        e.preventDefault();
-        var $this = $(this);
-        $topLevelMenuItems.slideToggle({
+    var $expandBtn = $('.mobile-expand > a'),
+        $mainMenuItems = $('.menu li').not('.branding, .mobile-expand'),
+        mobileAnimationDefaults = {
             'duration': 50,
             'easing': 'linear'
-        });
+        };
+
+    // Handle 'Expand' button (only shown on mobile screens)
+    $expandBtn.on('click', function (e) {
+        e.preventDefault();
+        $mainMenuItems.slideToggle(mobileAnimationDefaults);
     });
+
 })(window, jQuery);
