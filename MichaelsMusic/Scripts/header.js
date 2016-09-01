302c4e986522747463db6bc0c52dad982c564c60
Michael Pellegrini <mipelleg@microsoft.com>
2016-09-01 16:57:52 -0700

    Show/Hide submenus on click

--- MichaelsMusic/Scripts/header.js
+++ MichaelsMusic/Scripts/header.js
@@ -1,8 +1,11 @@
 (function(global, $) {
     'use strict';
+
     var $expandBtn = $('.mobile-expand > a'),
         $mainMenuItems = $('.menu li').not('.branding, .mobile-expand'),
-        mobileAnimationDefaults = {
+        $submenuHeadings = $('.has-submenu > a[href="#"]'),
+        $submenus = $('.submenu'),
+        animationDefaults = {
             'duration': 50,
             'easing': 'linear'
         };
@@ -10,7 +13,13 @@
     // Handle 'Expand' button (only shown on mobile screens)
     $expandBtn.on('click', function (e) {
         e.preventDefault();
-        $mainMenuItems.slideToggle(mobileAnimationDefaults);
+        $mainMenuItems.slideToggle(animationDefaults);
+    });
+
+    // Show submenu when clicking a menu heading
+    $submenuHeadings.on('click', function (e) {
+        e.preventDefault();
+        $(this).next('.submenu').slideToggle(animationDefaults);
     });
 
 })(window, jQuery);
