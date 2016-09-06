d04f38d0059be8cdb79c5a4a1972dd4a08348993
Michael Pellegrini <mipelleg@microsoft.com>
2016-09-05 17:11:00 -0700

    Edit some model props to be nullable

--- MichaelsMusic/Models/Guitar.cs
+++ MichaelsMusic/Models/Guitar.cs
@@ -2,29 +2,28 @@ namespace MichaelsMusic.Models
 {
     public class Guitar : Item
     {
-        public bool CarvedTop { get; set; }
-        public bool CoilTap { get; set; }
-        public bool IsHardtail { get; set; }
-        public bool IsTopJack { get; set; }
-        public decimal ScaleLength { get; set; }
-        public int FingerboardRadius { get; set; }
-        public int Frets { get; set; }
-        public int Strings { get; set; }
         public string Binding { get; set; }
         public string BodyMaterial { get; set; }
         public string BodyStyle { get; set; }
         public string Bridge { get; set; }
         public string BridgePickup { get; set; }
         public string Capacitors { get; set; }
+        public bool CarvedTop { get; set; }
+        public bool CoilTap { get; set; }
         public string Controls { get; set; }
+        public int? FingerboardRadius { get; set; }
         public string Finish { get; set; }
         public string Fretboard { get; set; }
+        public int? Frets { get; set; }
         public string Fretwire { get; set; }
         public string Headstock { get; set; }
         public string InlayMaterial { get; set; }
         public string InlayShape { get; set; }
+        public bool IsHardtail { get; set; }
+        public bool IsTopJack { get; set; }
         public string Knobs { get; set; }
         public string MiddlePickup { get; set; }
+        public string NeckBinding { get; set; }
         public string NeckFinish { get; set; }
         public string NeckJoint { get; set; }
         public string NeckMaterial { get; set; }
@@ -32,6 +31,8 @@ namespace MichaelsMusic.Models
         public string NeckShape { get; set; }
         public string Nut { get; set; }
         public string PickupSelector { get; set; }
+        public string ScaleLength { get; set; }
+        public int? Strings { get; set; }
         public string Tuners { get; set; }
     }
 }
